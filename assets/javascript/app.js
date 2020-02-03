8c0852235d8ec0e6ced2b6e8339d9f20049eedb2
Pedro Pinto <pedropinto.web@gmail.com>
2020-02-03 22:49:28 +0000

    Add multiple locations to pick from
    Added the most used stadiums
    Cleaned some unused code
    Refactored some JS
    Slighlty updated looks

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,17 +3,87 @@ const app = {
     baseCorsUrl: "https://cors-anywhere.herokuapp.com/",
     locations: [
         {
-            name: "Estádio do Dragão",
+            name: "Porto—Estádio do Dragão",
             id: 300,
         },
         {
-            name: "Estádio do Rio Ave",
+            name: "Lisboa—Estádio do Sport Lisboa e Benfica",
+            id: 101,
+        },
+        {
+            name: "Braga—Estádio Municipal de Braga",
+            id: 810,
+        },
+        {
+            name: "Lisboa—Estádio José Alvalade",
+            id: 818,
+        },
+        {
+            name: "Famalicão—Estádio Municipal de Famalicão",
+            id: 1171,
+        },
+        {
+            name: "Vila do Conde—Estádio do Rio Ave Futebol Clube",
             id: 727,
+        },
+        {
+            name: "Guimarães—Estádio D. Afonso Henriques",
+            id: 364,
+        },
+        {
+            name: "Porto—Estádio do Bessa Século XXI",
+            id: 120,
+        },
+        {
+            name: "Setúbal—Estádio do Bonfim",
+            id: 910,
+        },
+        {
+            name: "Tondela—Complexo Desportivo João Cardoso",
+            id: 1961,
+        },
+        {
+            name: "Ponta Delgada—Estádio de São Miguel",
+            id: 765,
+        },
+        {
+            name: "Barcelos—Estádio Cidade de Barcelos",
+            id: 342,
+        },
+
+        {
+            name: "Moreira de Cónegos—Estádio Comendador Almeida Freitas",
+            id: 575,
+        },
+
+        {
+            name: "Funchal—Estádio do Marítimo",
+            id: 544,
+        },
+
+        {
+            name: "Lisboa—Estádio do Restelo",
+            id: 99,
+        },
+
+        {
+            name: "Paços de Ferreira—Estádio da Capital do Móvel",
+            id: 651,
+        },
+
+        {
+            name: "Portimão—Estádio Municipal de Portimão",
+            id: 687,
+        },
+
+        {
+            name: "Vila das Aves—Estádio do Clube Desportivo das Aves",
+            id: 228,
         },
     ],
 
-    getData: async () => {
-        return fetch(`${app.baseCorsUrl}${app.baseSourceUrl}${app.activeLocation.id}`, {
+    getData: async selectedLocation => {
+        return fetch(`${app.baseCorsUrl}${app.baseSourceUrl}${selectedLocation}`, {
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded",
             },
@@ -43,110 +113,204 @@ const app = {
     createSchedule: table => {
         const gamesTable = table.querySelectorAll("tbody > tr")
 
-        return [...gamesTable].map(game => {
-            const allRows = game.querySelectorAll("td")
-            const dateRow = allRows[0]
-            const timeRow = allRows[2]
-            const homeTeamRow = allRows[3]
-            const visitorTeamRow = allRows[5]
+        return [...gamesTable]
+            .map(game => {
+                const allRows = game.querySelectorAll("td")
+                const dateRow = allRows[0]
+                const timeRow = allRows[2]
+                const homeTeamRow = allRows[3]
+                const visitorTeamRow = allRows[5]
 
-            // Date
-            const dateRegex = /(\d+)-(\d+)-(\d+)/
-            const dateMatch = dateRow.innerText.match(dateRegex)
-            const time = timeRow.innerText
-            const date = new Date(`${dateMatch[2]}-${dateMatch[1]}-${dateMatch[3]}`) // MM-DD-YYYY
-            const id = new Date(`${dateMatch[2]}-${dateMatch[1]}-${dateMatch[3]} ${time}`).getTime() // MM-DD-YYYY HH:MM
+                // Date
+                const dateRegex = /(\d+)-(\d+)-(\d+)/
+                const dateMatch = dateRow.innerText.match(dateRegex)
+                const time = timeRow.innerText
+                const date = new Date(`${dateMatch[2]}-${dateMatch[1]}-${dateMatch[3]}`) // MM-DD-YYYY
+                const id = new Date(
+                    `${dateMatch[2]}-${dateMatch[1]}-${dateMatch[3]} ${time}`
+                ).getTime() // MM-DD-YYYY HH:MM
 
-            // Teams
-            const homeTeam = app.cleanTeamNames(homeTeamRow.innerText)
-            const visitorTeam = app.cleanTeamNames(visitorTeamRow.innerText)
+                // Teams
+                const homeTeam = app.cleanTeamNames(homeTeamRow.innerText)
+                const visitorTeam = app.cleanTeamNames(visitorTeamRow.innerText)
 
-            return {
-                id,
-                date,
-                time,
-                homeTeam,
-                visitorTeam,
-            }
-        })
+                return {
+                    id,
+                    date,
+                    time,
+                    homeTeam,
+                    visitorTeam,
+                }
+            })
+            .filter(game => !/(\s?\[.+\])$/.test(game.homeTeam))
     },
 
     cleanTeamNames: team => {
-        // return team.trim().replace(/(\s?\[.+\])/, "")
         return team.trim()
     },
 
-    getGamesForToday: games => {
-        if (!games.length) {
-            return []
-        }
-
-        const today = new Date().setHours(0, 0, 0, 0)
-
-        return games.filter(game => game.date.getTime() === today)
+    getGameForToday: games => {
+        return !games.length
+            ? []
+            : games.filter(game => game.date.getTime() === app.today.setHours(0, 0, 0, 0))
     },
 
-    displayGamesForToday: games => {
+    displayGameForToday: games => {
         const mainGame = games[0]
 
-        app.elements.answer.innerText = "Há…"
+        app.elements.answer.innerText = "Há… 🙁"
         app.elements.answer.classList.add("app__answer--positive")
+        app.elements.app.classList.add("app--has-answer")
         app.elements.time.innerText = mainGame.time
-        app.elements.location.innerText = app.activeLocation.name
         app.elements.homeTeam.innerText = mainGame.homeTeam
         app.elements.visitorTeam.innerText = mainGame.visitorTeam
-
-        if (games.length > 1) {
-            const otherGames = games
-                .filter(game => game.id != mainGame.id)
-                .map(
-                    game =>
-                        `<li class="app__other-game">${game.homeTeam} vs ${game.visitorTeam} @ ${game.time}</li>`
-                )
-
-            app.elements.otherGames.innerHTML = otherGames
-            app.elements.body.classList.add("app--multiple-games")
-        }
     },
 
     displayNoGame: () => {
-        app.elements.answer.innerText = "Não!"
-        app.elements.location.innerText = app.activeLocation.name
+        app.elements.answer.innerText = "Não! 🎉"
         app.elements.answer.classList.add("app__answer--negative")
-        app.elements.body.classList.add("app--no-game-for-today")
+        app.elements.app.classList.add("app--has-answer")
     },
 
-    init: () => {
-        app.activeLocation = app.locations[0]
-        app.elements = {
-            body: document.querySelector("body"),
-            answer: document.querySelector(".js-answer"),
-            time: document.querySelector(".js-time"),
-            location: document.querySelector(".js-location"),
-            homeTeam: document.querySelector(".js-hometeam"),
-            visitorTeam: document.querySelector(".js-visitorteam"),
-            otherGames: document.querySelector(".js-othergames"),
-            loading: document.querySelector(".js-loading"),
+    fillLocationsDropdown: locations => {
+        const optionsMarkup = locations
+            .sort((locationA, locationB) => locationA.name.localeCompare(locationB.name))
+            .map(location => `<option value="${location.id}">${location.name}</option>`)
+
+        app.elements.locations.insertAdjacentHTML("beforeend", optionsMarkup)
+    },
+
+    setUpEvents: () => {
+        app.elements.locations.addEventListener("change", app.onChangeLocation)
+    },
+
+    isLoading: () => {
+        app.elements.app.classList.add("app--is-loading")
+        app.elements.app.classList.remove("app--has-answer")
+    },
+
+    hasLoaded: () => {
+        app.elements.app.classList.remove("app--is-loading")
+    },
+
+    clearDetails: () => {
+        app.elements.answer.innerText = ""
+        app.elements.answer.classList.remove("app__answer--positive", "app__answer--negative")
+
+        app.elements.app.classList.remove("app--has-answer")
+        app.elements.time.innerText = ""
+
+        app.elements.homeTeam.innerText = ""
+        app.elements.visitorTeam.innerText = ""
+    },
+
+    getTodayDate: () => {
+        app.today = new Date()
+        app.elements.today.innerText = app.today.toLocaleDateString("PT", {
+            month: "short",
+            day: "numeric",
+            year: "numeric",
+        })
+    },
+
+    updatePageTitle: title => {
+        document.title = `${title} | ${app.defaultTitle}`
+    },
+
+    updateURL: (id, title) => {
+        const newURL = `${window.location.origin}?estadio=${id}`
+        console.log(newURL, title, id)
+        window.history.pushState(
+            {
+                id,
+            },
+            title,
+            newURL
+        )
+    },
+
+    autoSearch: () => {
+        const locationIdMatch = window.location.search.match(/\d+/)
+
+        if (locationIdMatch && locationIdMatch.length) {
+            const locationId = locationIdMatch[0]
+            const locationIsValid =
+                app.locations.filter(location => String(location.id) === locationId).length > 0
+
+            if (!locationIsValid) {
+                return
+            }
+
+            app.isLoading()
+            app.getDataForLocation(locationId)
+
+            for (const location of app.elements.locations.options) {
+                if (location.value === locationId) {
+                    location.selected = true
+                }
+            }
+            app.updatePageTitle(
+                app.elements.locations.options[app.elements.locations.selectedIndex].innerText
+            )
         }
+    },
 
-        app.getData().then(data => {
+    getDataForLocation: locationId => {
+        app.getData(locationId).then(data => {
+            app.hasLoaded()
+
             if (data) {
-                app.elements.body.classList.add("app--loaded")
-
                 const games = app.parseContent(data)
 
                 app.games = app.createSchedule(games) || []
-                app.gamesForToday = app.getGamesForToday(app.games)
+                app.gameForToday = app.getGameForToday(app.games)
 
-                if (app.gamesForToday.length) {
-                    app.displayGamesForToday(app.gamesForToday)
+                if (app.gameForToday.length) {
+                    app.displayGameForToday(app.gameForToday)
                 } else {
                     app.displayNoGame()
+                    console.log("game data", app.games)
                 }
             } else {
-                app.elements.loading.innerText = "Erro no servidor…"
+                app.elements.error.innerText = "Erro no servidor…"
             }
         })
+    },
+
+    onChangeLocation: event => {
+        const locationId = event.target.value
+        console.log("location selected", event.target.value)
+        app.isLoading()
+        app.clearDetails()
+        app.updatePageTitle(
+            app.elements.locations.options[app.elements.locations.selectedIndex].innerText
+        )
+        app.updateURL(
+            event.target.value,
+            event.target.options[event.target.selectedIndex].innerText
+        )
+
+        app.getDataForLocation(locationId)
+    },
+
+    init: () => {
+        app.defaultTitle = document.title
+        app.elements = {
+            app: document.querySelector(".app"),
+            answer: document.querySelector(".js-answer"),
+            time: document.querySelector(".js-time"),
+            locations: document.querySelector(".js-locations"),
+            homeTeam: document.querySelector(".js-hometeam"),
+            visitorTeam: document.querySelector(".js-visitorteam"),
+            loading: document.querySelector(".js-loading"),
+            error: document.querySelector(".js-error"),
+            today: document.querySelector(".js-today"),
+        }
+
+        app.setUpEvents()
+        app.fillLocationsDropdown(app.locations)
+        app.getTodayDate()
+        app.autoSearch()
     },
 }
 
